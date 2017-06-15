57f7a5ec132d121e131b1da739973dfcacb83c82
Dereje Kitaw <dkitaw@solareworld.local>
2017-06-15 13:26:35 -0400

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, {ProtoTypes} from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            'App works'
+        );
+    }
+};
+
+export default App;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component{
+    render(){
+        return(
+            'HomePage works'
+        );
+    }
+};
+
+export default HomePage;
